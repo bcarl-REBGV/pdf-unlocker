7112cce3b2e156c2d3ec0cb497f3ac5092af26f1
Brad Carlson <brad@bscarlson.com>
2022-07-25 15:50:00 -0700

    Added button to open file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
 from kivy.lang.builder import Builder
 from kivy.core.window import Window
 from kivy.config import Config
@@ -19,56 +20,77 @@ from functools import partial
 Config.set('graphics', 'width', '300')
 Config.set('graphics', 'height', '300')
 
+locked_padlock = 'assets\\locked-padlock.png'
+unlocked_padlock = 'assets\\unlocked-padlock.png'
+spinner = 'assets\\spinner.gif'
+
+
 class OutputPath(EventDispatcher):
     value = StringProperty("")
+
 
 class RootWidget(FloatLayout):
     def __init__(self, **kwargs):
         super(RootWidget, self).__init__(**kwargs)
         with self.canvas.before:
-            Color(1,1,1,1)
+            Color(1, 1, 1, 1)
             self.rect = Rectangle(pos=self.pos, size=self.size)
         self.bind(size=self.update_size)
         Window.bind(on_drop_file=self._on_drop_file)
-        self.padlock = Image(source="locked-padlock.png", size_hint=(0.25,0.25), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        self.padlock = Image(source=locked_padlock, size_hint=(
+            0.25, 0.25), pos_hint={'center_x': 0.5, 'center_y': 0.55})
         self.add_widget(self.padlock)
-        self.text = Label(text="Drop PDF here to unlock.", size_hint=(1, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.3}, color=(0,0,0,1))
+        self.text = Label(text="Drop PDF here to unlock.", size_hint_y=None, text_size=(
+            self.width * 5, None), pos_hint={'center_x': 0.5, 'center_y': 0.35}, color=(0, 0, 0, 1), halign="center")
+        self.text.height = self.text.texture_size[1]
         self.add_widget(self.text)
-    
+        self.button = Button(text="Open", size_hint=(0.25, 0.1), pos_hint={
+                             'center_x': 0.5, 'center_y': 0.15})
+        self.button.bind(on_press=self.open_file)
+
     def update_size(self, instance, value):
         self.rect.pos = self.pos
         self.rect.size = self.size
+
     def _on_drop_file(self, window, file_path, x, y):
-        self.padlock.source = "spinner.gif"
-        self.padlock.size_hint=(1,1)
+        self.padlock.source = spinner
+        self.padlock.size_hint = (1, 1)
         self.target_file_path = file_path
+        self.text.text = ""
         output = OutputPath()
         output.value = ""
         output.bind(value=self.unlock_finished)
         thread = Thread(target=self.dispatch_unlock, args=(file_path, output))
         thread.start()
-        
+
     def dispatch_unlock(self, file_path, output):
         output.value = unlockPdf(file_path)
-        
+        self.output_file_path = output.value
+
     def unlock_finished(self, instance, value):
         Clock.schedule_once(partial(self.unlock_update, value), 1)
-    
+
     def unlock_update(self, value, dt):
-        self.padlock.source = "unlocked-padlock.png"
-        self.padlock.size_hint=(0.25,0.25)
-        os.startfile(value)
-        
+        self.padlock.source = unlocked_padlock
+        self.text.text = self.output_file_path
+        self.padlock.size_hint = (0.25, 0.25)
+        self.add_widget(self.button)
+
+    def open_file(self, instance,):
+        os.startfile(self.output_file_path)
+
     def reset_ui(self):
         self.padlock.source = 'locked-padlock.png'
 
 
-class TestDropApp(App):
+class PdfUnlockApp(App):
     def build(self):
         Window.size = (400, 400)
         # Window.bind(on_drop_file=self._on_drop_file)
         Window.minimum_width, Window.minimum_height = (400, 200)
         return RootWidget()
 
+
 if __name__ == "__main__":
-    TestDropApp().run()
+    app = PdfUnlockApp()
+    app.run()
